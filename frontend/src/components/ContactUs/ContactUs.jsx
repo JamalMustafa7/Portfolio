1183048596345dd3bf4bef4f2e82f9c6f07c4f8c
JamalMustafa7 <muhammedjamalmustafa@gmail.com>
2024-08-22 14:55:10 +0500

    Links resolved

--- frontend/src/components/ContactUs/ContactUs.jsx
+++ frontend/src/components/ContactUs/ContactUs.jsx
@@ -8,13 +8,13 @@ const ContactUs = () => {
       <h2>Contact Us</h2>
       <p>Feel free to reach out to us through any of the following platforms:</p>
       <div className={styles.contactIcons}>
-        <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/JamalMustafa7/" target="_blank" rel="noopener noreferrer">
           <FaGithub className={`${styles.icon} ${styles.githubIcon}`} />
         </a>
-        <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/muhammad-jamal-mustafa-2021cs631/" target="_blank" rel="noopener noreferrer">
           <FaLinkedin className={`${styles.icon} ${styles.linkedinIcon}`} />
         </a>
-        <a href="mailto:yourname@example.com">
+        <a href="mailto:muhammedjamalmustafa@gmail.com">
           <FaEnvelope className={`${styles.icon} ${styles.emailIcon}`} />
         </a>
       </div>
